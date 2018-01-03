0dcf267dc7270a6cc7415057e55120ee37de1f69
michael glembotzki <michael.glembotzki@irisgmbh.de>
2018-01-03 08:25:25 +0100

    fix core_control.c

--- arch/arm/mach-sc57x/core_control.c
+++ arch/arm/mach-sc57x/core_control.c
@@ -16,86 +16,105 @@
 
 #define CMD_CORE_START         _IO('b', 0)
 #define CMD_CORE_STOP          _IO('b', 1)
+#define CMD_CORE_SET_VSTART_1  _IO('b', 2)
+#define CMD_CORE_SET_VSTART_2  _IO('b', 3)
+
 #define VALID_CORE_MIN         1
 #define VALID_CORE_MAX         2
 
-void adi_core_start(unsigned int coreid)
+
+static long adi_core_stop(unsigned int coreid)
 {
-/* clear CRSTAT bit for given coreid */
-	writel(1 << coreid, __io_address(REG_RCU0_CRSTAT));
-	if (!(readl(__io_address(REG_RCU0_CRCTL)) & 1 << coreid)) {
+	long ret = 0;
+	if (coreid < VALID_CORE_MIN || coreid > VALID_CORE_MAX) {
+		ret = -EINVAL;
+	} else {
+		if (readl(__io_address(REG_RCU0_CRCTL)) & 1 << coreid) {
+			pr_info("corectl core %d is already unter reset\n", coreid);
+			return 0;
+		}
+		/* clear CRSTAT bit for given coreid */
+		writel(1 << coreid, __io_address(REG_RCU0_CRSTAT));
 		/* disable the system interface */
-		writel(readl(__io_address(REG_RCU0_SIDIS)) | 1 << (coreid-1),
-			   __io_address(REG_RCU0_SIDIS));
+		writel(readl(__io_address(REG_RCU0_SIDIS)) | 1 << (coreid - 1),
+				__io_address(REG_RCU0_SIDIS));
 		/* Anomaly 36-10-0005: SISTAT doesn't acknowledge set status */
-		/* while(readl(__io_address(REG_RCU0_SISTAT) &
-		 *             1 << (coreid-1)!=1); */
+		/* while (readl(REG_RCU0_SISTAT) & 1 << (coreid - 1) != 1); */
 		udelay(50);
 		/* put core in reset */
 		writel(readl(__io_address(REG_RCU0_CRCTL)) | 1 << coreid,
-			   __io_address(REG_RCU0_CRCTL));
+				__io_address(REG_RCU0_CRCTL));
 		/* reenable the system interface */
-		writel(readl(__io_address(REG_RCU0_SIDIS)) & ~(1 << (coreid-1)),
-			   __io_address(REG_RCU0_SIDIS));
+		writel(readl(__io_address(REG_RCU0_SIDIS)) & ~(1 << (coreid - 1)),
+				__io_address(REG_RCU0_SIDIS));
 		/* Anomaly 36-10-0005: SISTAT doesn't acknowledge set status */
-		/* while(readl(__io_address(REG_RCU0_SISTAT) &
-		 *             1 << (coreid-1)!=0); */
+		/* while (readl(REG_RCU0_SISTAT) & 1 << (coreid - 1) != 0); */
 		udelay(50);
+		/* clear CRSTAT bit for given coreid */
+		writel(1 << coreid, __io_address(REG_RCU0_CRSTAT));
 	}
-	/* move core out of reset */
-	writel(readl(__io_address(REG_RCU0_CRCTL)) & ~(1 << coreid),
-			__io_address(REG_RCU0_CRCTL));
-	/* clear CRSTAT bit for given coreid */
-	writel(1 << coreid, __io_address(REG_RCU0_CRSTAT));
-	/* notify CCES */
-	writel(1 << (18 + coreid), __io_address(REG_RCU0_MSG_SET));
+	return ret;
 }
 
-void adi_core_stop(unsigned int coreid)
+static long adi_core_start(unsigned int coreid)
 {
-	if (readl(__io_address(REG_RCU0_CRCTL)) & 1 << coreid)
-		return;
-	/* clear CRSTAT bit for given coreid */
-	writel(1 << coreid, __io_address(REG_RCU0_CRSTAT));
-	/* disable the system interface */
-	writel(readl(__io_address(REG_RCU0_SIDIS)) | 1 << (coreid - 1),
-			__io_address(REG_RCU0_SIDIS));
-	/* Anomaly 36-10-0005: SISTAT doesn't acknowledge set status */
-	/* while (readl(REG_RCU0_SISTAT) & 1 << (coreid - 1) != 1); */
-	udelay(50);
-	/* put core in reset */
-	writel(readl(__io_address(REG_RCU0_CRCTL)) | 1 << coreid,
-			__io_address(REG_RCU0_CRCTL));
-	/* reenable the system interface */
-	writel(readl(__io_address(REG_RCU0_SIDIS)) & ~(1 << (coreid - 1)),
-			__io_address(REG_RCU0_SIDIS));
-	/* Anomaly 36-10-0005: SISTAT doesn't acknowledge set status */
-	/* while (readl(REG_RCU0_SISTAT) & 1 << (coreid - 1) != 0); */
-	udelay(50);
-	/* clear CRSTAT bit for given coreid */
-	writel(1 << coreid, __io_address(REG_RCU0_CRSTAT));
+	long ret = 0;
+	if (coreid < VALID_CORE_MIN || coreid > VALID_CORE_MAX) {
+		ret = -EINVAL;
+	} else {
+		ret = adi_core_stop(coreid);
+		if (ret != 0) {
+			return ret;
+		}
+		/* move core out of reset */
+		writel(readl(__io_address(REG_RCU0_CRCTL)) & ~(1 << coreid),
+				__io_address(REG_RCU0_CRCTL));
+		/* clear CRSTAT bit for given coreid */
+		writel(1 << coreid, __io_address(REG_RCU0_CRSTAT));
+		/* notify CCES */
+		writel(1 << (18 + coreid), __io_address(REG_RCU0_MSG_SET));
+	}
+	return ret;
+}
+
+
+/*
+ * set the address of the first instruction to execute for coreid
+ */
+static long core_set_svector(unsigned int coreid, unsigned long addr)
+{
+	if (1 == coreid) {
+		writel(addr, __io_address(REG_RCU0_SVECT1));
+	} else if (2 == coreid) {
+		writel(addr, __io_address(REG_RCU0_SVECT2));
+	} else {
+		return -EINVAL;
+	}
+	return 0;
 }
 
 static long core_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
-		int ret = 0;
+	int ret = 0;
 
-		if (arg < VALID_CORE_MIN || arg > VALID_CORE_MAX)
-			ret = -EINVAL;
-		else
-			switch (cmd) {
-			case CMD_CORE_START:
-				adi_core_start(arg);
-				break;
-			case CMD_CORE_STOP:
-				adi_core_stop(arg);
-				break;
-			default:
-				ret = -EINVAL;
-				break;
-			}
-
-		return ret;
+	switch (cmd) {
+	case CMD_CORE_START:
+		ret = adi_core_start(arg);
+		break;
+	case CMD_CORE_STOP:
+		ret = adi_core_stop(arg);
+		break;
+	case CMD_CORE_SET_VSTART_1:
+		ret = core_set_svector(1, arg);
+		break;
+	case CMD_CORE_SET_VSTART_2:
+		ret = core_set_svector(2, arg);
+		break;
+	default:
+		ret = -EINVAL;
+		break;
+	}
+	return ret;
 }
 
 static const struct file_operations core_fops = {
