f9567e7fa97168512a4716bb80593c977a5d72fe
philipp.schoewe <philipp.schoewe@iris-sensing.com>
2022-03-31 12:06:58 +0200

    debug output

--- drivers/pinctrl/pinctrl-adi2.c
+++ drivers/pinctrl/pinctrl-adi2.c
@@ -279,6 +279,8 @@ static void adi_gpio_ack_irq(struct irq_data *d)
 	}
 
 	writel(pintbit, &regs->request);
+	
+	printk(KERN_INFO "adi_gpio_ack_irq: pintbit=%i hwirq=%i invert_set=%i invert_clear=%i", pintbit, d->hwirq, regs->invert_set, regs->invert_clear);
 
 	spin_unlock(&port->pint->lock);
 	spin_unlock_irqrestore(&port->lock, flags);
@@ -303,6 +305,8 @@ static void adi_gpio_mask_ack_irq(struct irq_data *d)
 
 	writel(pintbit, &regs->request);
 	writel(pintbit, &regs->mask_clear);
+
+	printk(KERN_INFO "adi_gpio_mask_ack_irq: pintbit=%i hwirq=%i invert_set=%i invert_clear=%i", pintbit, d->hwirq, regs->invert_set, regs->invert_clear);
 
 	spin_unlock(&port->pint->lock);
 	spin_unlock_irqrestore(&port->lock, flags);
